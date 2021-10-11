76edfe5689f2db9212be31bcea15e376c8cc20b7
olgavasina-www <olgavasgo@yandex.ru>
2021-10-11 19:43:37 +0300

    first python commit

---
+++ 1_210921.py
@@ -0,0 +1,19 @@
+#создать список на 20 чисел, циклом for вывести в консоль этот список
+
+list1 = list([32,12,3,4,8,5,3,1,9,3,0,3,5,7,3,54,61,77,34,90])
+
+#фунция - узнать длинну(количество элеменов в последовательности)списка
+#len()
+
+array = [12, 89, 0, 4]
+
+lengthOfArray = len(array)
+#print(lengthOfArray)
+######################
+
+dictionary ={"first": "value", "second": "value2", "third": "value3"}
+lenghtOfDictionary = len(dictionary)
+print(lenghtOfDictionary)
+
+#for i in range(len(list1)):
+#    print(list1[i])
